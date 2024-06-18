b6a25805fef8e856feab7fbe8e5a51c8fc21af70
itsalwaysSK <sabnamskc@gmail.com>
2024-06-18 08:09:04 +0545

    Add files via upload

---
+++ greaterORless.js
@@ -0,0 +1,10 @@
+var a=10;
+if(a<50){
+    console.log(`The number ${a} is less than 50.`);
+}
+else if(a>50){
+    console.log(`The number ${a} is greater than 50.`);
+}
+else{
+    console.log(`The number ${a} ia equal to 50.`)
+}
---
+++ oddOReven.js
@@ -0,0 +1,7 @@
+var a = 9;
+if(a % 2 ==0){
+    console.log(`The given number ${a} is even`);
+}
+else{
+    console.log(`The given number ${a} is odd.`);
+}

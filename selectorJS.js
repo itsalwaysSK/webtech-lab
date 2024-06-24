725e5bb8449398341c8426c6635031de87646e7b
itsalwaysSK <sabnamskc@gmail.com>
2024-06-24 08:03:06 +0545

    Add files via upload

---
+++ selectorJS.js
@@ -0,0 +1,7 @@
+function add(){
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+    const sum = Number(num1) + Number(num2);
+    document.getElementById('output').value;
+    output.innerHTML = sum;
+}
